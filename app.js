b3e3b77c70f4c3b2387f5053e04e925cab796493
MirzaDSulistyo <mirzadanu@ymail.com>
2019-01-15 22:25:36 +0700

    handle error response

--- app.js
+++ app.js
@@ -58,8 +58,11 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).send({
+    status: err.status,
+    message: err.message
+  });
+  //res.render('error');
 });
 
 module.exports = app;
