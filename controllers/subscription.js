37b133677e744d9fd632bab0198b547268fa4857
MirzaDSulistyo <mirzadanu@ymail.com>
2019-02-24 18:28:56 +0700

    populate product on subs

--- controllers/subscription.js
+++ controllers/subscription.js
@@ -54,17 +54,19 @@ exports.list = function(req, res, next) {
     if (err) return res.status(500).send({ status: 500, message: "There was a problem finding list of subscription." });
     if (!data) return res.status(404).send({ status: 404, message: "No subscription found." });
     
-    /*async.forEach(data,function(item,callback) {
-        Product.find({}).populate(item.plan,{ "path": "products" },function(err,output) {
-            if (err) return res.status(500).send({ status: 500, message: "There was a problem finding list of products." });
+    async.forEach(data,function(item,callback) {
+        Product.populate(item.plan,{ "path": "products" },function(err,output) {
+            if (err) throw err;
+            callback();
         });
     }, function(err) {
+        if (err) return res.status(500).send({ status: 500, message: "There was a problem finding list of products." });
         res.status(200).send({ status: 200, data: data });
-    });*/
+    });
 
-    res.status(200).send({ status: 200, data: data });
+    //res.status(200).send({ status: 200, data: array });
   })
-}
+}   
 
 exports.detail = function(req, res, next) {
 	Subs.findById(req.params.id)
